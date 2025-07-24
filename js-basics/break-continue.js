181e3f1d8116891034aff4476fce8f5263aed8a0
Flebory <123499686+Flebory@users.noreply.github.com>
2025-07-24 10:11:58 -0400

    Uploading solution for break continue

--- js-basics/break-continue.js
+++ js-basics/break-continue.js
@@ -1,1 +1,26 @@
-h
+function grabDoll(dolls){
+  var bag=[];
+  
+  for(var i = 0; i < dolls.length; i++) {
+
+    if(dolls[i] == "Barbie doll"){
+        bag.push(dolls[i]);
+        console.log(bag);
+      } else if(dolls[i] == "Hello Kitty"){
+        bag.push(dolls[i])
+        console.log(bag);
+      } else {
+        continue;
+      }
+    
+    if(bag.length < 3){
+      continue;
+    } else {
+      break;
+    }
+  }
+  return bag;
+}
+
+// My brute force solution of adding hello kitty / barbie doll elements into bag array
+// Can simplify using other operators to compare the elements within the bag array to make it more effiecient
