7dde16f639a25eb026e6ba5bed6d76ac46d78978
Gregory Dort <gregory.dort@gmail.com>
2025-07-24 22:05:08 -0400

    Uploaded for in and for of solution

--- js-basics/for in and for of.js
+++ js-basics/for in and for of.js
@@ -1,1 +1,20 @@
 // For in and For of solution
+function giveMeFive(obj){
+  let arr = [];
+  
+  for(var key in obj){
+    if(key.length == 5){
+      arr.push(key);
+    }
+    
+    var value = obj[key];
+    if(value.length == 5){
+      arr.push(value);
+    }
+  }
+  return arr;
+}
+
+// Utilized for loop to iterate through keys in object
+// Used if statements to check the length of the key itself and the value associated with it within one iteration
+// This is efficient because it can check both the key and value in one pass through the object
