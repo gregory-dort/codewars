265add976e47501e7f0ed8f8c0d20d7169a48b39
Gregory Dort <gregory.dort@gmail.com>
2025-07-29 20:03:48 -0400

    Updated concept questions

--- concept-practice/if-else/emp-bonus.js
+++ concept-practice/if-else/emp-bonus.js
@@ -2,8 +2,9 @@
 // Employee bonus based on years of service to company
 // If employee has 1 year or less, bonus is $100
 // if employee has between 1 and 5 years, bonus is $500
-// If emplyee has more than 5 yars, bonus is $1000
+// If employee has more than 5 yars, bonus is $1000
+// yearsOfService is a number
 
 function calculateBonus(yearsOfService) {
-    
+
 }
