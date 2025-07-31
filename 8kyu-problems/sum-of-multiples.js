c55d219a3bed051963ba86d720c46e368fda1f2d
Gregory Dort <gregory.dort@gmail.com>
2025-07-31 19:08:30 -0400

    Uploading sum of multiples solution

---
+++ 8kyu-problems/sum-of-multiples.js
@@ -0,0 +1,11 @@
+function sumMul(n,m){
+  if(n <= 0 || m <= 0){return "INVALID"}
+  var mult = [];
+  var sum = 0;
+  
+  for(var i = n; i < m; i += n){
+    mult.push(i);
+  }
+  
+  return mult.reduce((accumulator, currentValue) => accumulator + currentValue);
+}
