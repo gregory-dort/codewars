918ef8cb15709296d204c8f80588592e9f070022
Flebory <123499686+Flebory@users.noreply.github.com>
2025-07-31 11:29:25 -0400

    Uploading sueface area / volume of box solution

--- 8kyu-problems/surface-area-volume-box.js
+++ 8kyu-problems/surface-area-volume-box.js
@@ -1,6 +1,13 @@
 // Problem: Find the surface area and volume of a box based on three positive non-zero integers
 // Level: 8kyu
 
-function getSize(width, height, depth) {
-    
+function getSize(width, height, length) {
+  var result = [];
+  
+  var calcArea = 2 * width * length + 2 * height * width + 2 * length * height;
+  result.push(calcArea);
+  var calcVolume = width * length * height;
+  result.push(calcVolume);
+  
+  return result;
 }
