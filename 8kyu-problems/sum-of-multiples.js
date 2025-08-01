a4959bdab3600772d8bee2e319a7bafe1f8a8e43
Flebory <123499686+Flebory@users.noreply.github.com>
2025-08-01 17:52:30 -0400

    Uploading revision to sum of multiples

--- 8kyu-problems/sum-of-multiples.js
+++ 8kyu-problems/sum-of-multiples.js
@@ -1,7 +1,7 @@
 function sumMul(n,m){
   if(n <= 0 || m <= 0){return "INVALID"}
   var mult = [];
-  var sum = 0;
+  var sum = 0; // Not needed here as it isn't returned or used in function
   
   for(var i = n; i < m; i += n){
     mult.push(i);
