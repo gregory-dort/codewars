349dd90016ae45064fc962450683bbc0be7f3640
Gregory Dort <gregory.dort@gmail.com>
2025-10-07 10:11:53 -0400

    Uploading leap-years solution

---
+++ 7kyu-problems/leap-years.js
@@ -0,0 +1,13 @@
+function isLeapYear(year) {
+  if (year % 400 == 0){
+    return true;
+  } else if (year % 100 == 0){
+    return false;
+  } else if (year % 4 == 0){
+    return true;
+  } else return false;
+}
+
+console.log(isLeapYear(2000)); //true
+console.log(isLeapYear(1900)); //false
+console.log(isLeapYear(2012)); //true
